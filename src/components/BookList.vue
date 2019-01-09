5de1fa4916e98733a59788b6780d10e2e912cb4c
caetanomb <caetanomb@gmail.com>
2019-01-09 16:24:51 +0100

    Second commit - Looping through a list

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -1,20 +1,47 @@
 <template>
     <div>
         <h1>{{ title }}</h1>
-    </div>
+        <ul>
+            <li v-for="book in books">{{book.title}}:{{book.author}}</li>            
+        </ul>
+    </div>    
 </template>
 <script>
     export default {
         name: 'BookList',
         data() {
             return {
-                title: 'All Books'
+                title: 'All Books',
+                books: [
+                    {
+                        title: 'Self-Reliance',
+                        author: 'Ralph Waldo Emerson'
+                    },
+                    {
+                        title: 'American Gods',
+                        author: 'Neil Gaiman'
+                    },
+                    {
+                        title: 'Amusing Ourselves to Death',
+                        author: 'Neil Postman'
+                    }
+                ]
             }
         }
     }
 </script>
 <style>
 h1, h2 {
-    font-weight: normal
+    font-weight: normal;
+}
+
+ul {
+    list-style-type: none;
+    padding: 0px
+}
+
+li {
+    display: block;
+    margin: 10px;
 }
 </style>
