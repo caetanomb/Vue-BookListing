6153a489cd67b607cb314dd8cab7bb44c9e9d76f
caetanomb <caetanomb@gmail.com>
2019-01-10 09:29:49 +0100

    Item 4 first commit

---
+++ src/components/BookItem.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <li>{{ book.title}}:{{book.author}}</li>
+    </div>
+</template>
+<script>
+    export default {
+        name: "BookItem",
+        props: ['book']
+    }
+</script>
+<style>
+
+</style>
